1a46d5d6a8d3703571cca304f13809ef32656ee4
Nguyen Minh <109507529+contactgithubb@users.noreply.github.com>
2022-08-04 21:13:37 +0700

    Update WelcomeGit.java

--- src/WelcomeGit.java
+++ src/WelcomeGit.java
@@ -1,6 +1,6 @@
 public class WelcomeGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
-        System.out.println("Welcome people come with Git");
+        System.out.println("Welcome people come with Git for beginners");
     }
 }
